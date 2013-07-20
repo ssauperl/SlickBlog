8e451296f36d8f5e22f8ce6ab0e456108daf1ae5
Simon <simon.sauperl@gmail.com>
2013-07-20 18:40:20 +0200

    google auth enabled

--- SlickBlog.Web/App_Start/AuthConfig.cs
+++ SlickBlog.Web/App_Start/AuthConfig.cs
@@ -26,7 +26,7 @@ namespace SlickBlog.Web
             //    appId: "",
             //    appSecret: "");
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
