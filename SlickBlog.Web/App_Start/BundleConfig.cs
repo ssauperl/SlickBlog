bbd7f389984c4520e53a709ef32a5e06e3b31c98
Simon <simon.sauperl@gmail.com>
2013-07-31 10:34:56 +0200

    less rendering added to layout, packages updated

--- SlickBlog.Web/App_Start/BundleConfig.cs
+++ SlickBlog.Web/App_Start/BundleConfig.cs
@@ -8,20 +8,23 @@ namespace SlickBlog.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+            "~/Scripts/lib/jquery-{version}.js"
+            , "~/Scripts/lib/bootstrap.js"
+            , "~/Scripts/lib/knockout-{version}.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/lib/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/lib/jquery.unobtrusive*",
+                        "~/Scripts/lib/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
